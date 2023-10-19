2cc2064151f845e44f0f150b1d3b50617c39e8fe
jaylaye <yejayla69@gmail.com>
2023-10-19 00:39:38 -0400

    w

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const valAgents = [
     agentType: "Duelist",
     theyBad: "true",
     unlocked: "true",
-    origin: "mexico",
+    origin: "Mexico",
+    abilities: ["Ability1", "Ability2", "Ability3"],
 
 },
 {
@@ -12,7 +13,8 @@ const valAgents = [
     agentType: "Sentinel",
     theyBad: "true",
     unlocked: "true",
-    origin: "Philiphines",
+    origin: "Philippines",
+    abilities: ["Ability4", "Ability5", "Ability6"],
 },
 {
     agentName: "Harbor",
@@ -20,6 +22,7 @@ const valAgents = [
     theyBad: "false",
     unlocked: "false",
     origin: "India",
+    abilities: ["Ability7", "Ability8", "Ability9"],
 },
 {
     agentName: "Kay/O",
@@ -27,15 +30,15 @@ const valAgents = [
     theyBad: "false",
     unlocked:"false",
     origin: "unknown",
+    abilities: ["Ability10", "Ability11", "Ability12"],
 }
 ]
 
 valAgents.forEach((val) => {
     console.log(val.agentName);
+  });
 
-});
-valAgents.forEach((val) => {
-    console.log(val.agentType);
-});
-const agentsVal = valAgents.filter(val => val.agents === true);
+valAgents.forEach((agent) => agent.abilities.forEach((ability) => console.log(ability)));
+
+const agentsVal = valAgents.filter((val) => val.theyBad === "true");
 console.log(agentsVal);
